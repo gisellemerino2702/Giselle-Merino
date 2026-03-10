1cbd90de8f4757ad7b8e6ab0c17e2effb4fc45ea
Giselle <gisellemerino2702@gmail.com>
2026-03-10 16:02:44 -0600

    main guardado

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ for line in sys.stdin: # se recorre cada linea que llega desde stdin y el progra
     if line == "":
         print(0) #si la linea esta vacia o solo tiene espacios imprime 0 y continua a la siguiente linea.
         continue 
-    elementos = line.split(",") #separamos los elementos por comas.
+    valores = line.split(",") #separamos los elementos por comas.
     suma = 0 
-    for e in elementos: 
-        e = e.strip() #elimina espacios antes y despues de cada valor.
+    for valor in valores: 
+        valor = valor.strip() #elimina espacios antes y despues de cada valor.
         limpio = "" #limpia caracteres invalidos.
-        for c in e:
+        for c in valor:
             if c.isdigit() or c == "." or c == "-":
                 limpio += c
-            if limpio == "" or limpio == "-" or limpio == ".":
-                numero = 0
-            else:
-                numero = float(limpio)
-            numero = int(numero)  #truncar decimales        
-            suma += numero #suma todos los valores
+        if limpio == "" or limpio == "-" or limpio == ".":
+            numero = 0
+        else:
+            numero = float(limpio)
+        numero = int(numero)  #truncar decimales        
+        suma += numero #suma todos los valores
 
 print(suma)
