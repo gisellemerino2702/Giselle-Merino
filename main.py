347c790abbb430cd4032b68c2a764d3da2be151f
Giselle <gisellemerino2702@gmail.com>
2026-03-10 17:57:03 -0600

    main guardado el bueno (espero) ya por favor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys #para leer datos desde la entrada (stdin).
 import re #limpiar caracteres invalidos.
+print(f"INGRESE LOS VALORES SEPARADOS POR COMAS: \n")
 def sacar_num(texto):
     texto = texto.strip()
     limpio = re.sub(r"[^0-9\.-]", "", texto)
