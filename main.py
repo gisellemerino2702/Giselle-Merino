10befd2f9803ce478352d40bace4a6b4c9d4bd0f
Giselle <gisellemerino2702@gmail.com>
2026-03-10 16:58:00 -0600

    main guardado el bueno (espero)x3

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import sys #para leer datos desde la entrada (stdin).
 print(f"INGRESE VALORES SEPARADOS POR COMAS: \n")
-lines = sys.stdin.read().splitlines()
-for line in lines: # se recorre cada linea que llega desde stdin y el programa seguira leyendo hasta llegar al fin del archivo.
-    line = line.strip() #elimina espacios al inicio y al final de la linea.
-    if line == "":
+for linea in sys.stdin: # se recorre cada linea que llega desde stdin y el programa seguira leyendo hasta llegar al fin del archivo.
+    linea = linea.strip() #elimina espacios al inicio y al final de la linea.
+    if linea == "":
         print(0) #si la linea esta vacia o solo tiene espacios imprime 0 y continua a la siguiente linea.
         continue 
-    elementos = line.split(",") #separamos los elementos por comas.
-    suma_linea = 0 
-    for elemento in elementos: 
-        elemnto = elemento.strip() #elimina espacios antes y despues de cada valor.
+    valores = linea.split(",") #separamos los elementos por comas.
+    suma = 0 
+    for valor in valores: 
+        valor = valor.strip() #elimina espacios antes y despues de cada valor.
         limpio = "" #limpia caracteres invalidos.
-        for c in elemento:
+        for c in valor:
             if c.isdigit() or c == "." or c == "-":
                 limpio += c
         if limpio == "" or limpio == "-" or limpio == ".":
@@ -19,6 +18,6 @@ for line in lines: # se recorre cada linea que llega desde stdin y el programa s
         else:
             numero = float(limpio)
         numero = int(numero)  #truncar decimales        
-        suma_linea += numero #suma todos los valores
+        suma += numero #suma todos los valores
 
-print(suma_linea)
+print(suma)
