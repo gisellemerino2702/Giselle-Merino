dba0b486534ac42995468a82cc0fc3c5fb90bb67
Giselle <gisellemerino2702@gmail.com>
2026-03-20 21:48:39 -0600

    Reto 2 terminado

---
+++ Reto2/main.py
@@ -0,0 +1,46 @@
+import sys
+
+def fahrenheit_a_celsius(f):
+    """Convierte fahrenheit a celsius"""
+    return (f - 32) * 5/9
+ 
+def clasificar_temperatura(celsius):
+    """Clasifica las temperaturas segun rangos definidos"""
+    if celsius < 0:
+        return "Congelado"
+    elif celsius <= 15:
+        return "Frio"
+    elif celsius <= 25:
+        return "Calido"
+    else:
+        return "Extremo"
+    
+def main():
+    lineas = sys.stdin.read().strip().split("\n")
+
+    print("\nCiudad, temperatura_celsius, clasificacion")
+
+    for linea in lineas [1:]:
+        partes = linea.split(",")
+
+        if len(partes) != 3:
+            continue
+        ciudad, temp_str, unidad = partes
+
+        try:
+            temperatura = float(temp_str) 
+        except: 
+            continue
+
+        if unidad not in ["C", "F"]:
+            continue
+
+        if unidad == "F":
+            temperatura = fahrenheit_a_celsius(temperatura)
+
+        clasificacion = clasificar_temperatura(temperatura)
+
+        print(f"{ciudad},{temperatura:.1f},{clasificacion}")
+
+if __name__ == "__main__":
+    main() 
